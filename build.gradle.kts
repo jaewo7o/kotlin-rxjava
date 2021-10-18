e2aaa60050eb7f08a08ed81d9d2d271613027842
Jung Jaewoo <jaewo7o@naver.com>
2021-10-18 16:37:09 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.30"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.0.0"
+}
+
+group = "com.jaewoo.rxjava"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation("org.springframework:spring-webflux:5.3.10")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
---
+++ src/main/kotlin/com/jaewoo/rx/RxJava01.kt
@@ -0,0 +1,30 @@
+package com.jaewoo.rx
+
+import io.reactivex.Flowable
+import io.reactivex.Observable
+
+class RxJava01 {
+    fun basicConcept01() {
+        // 1. 배압처리 가능
+        // 데이터의 발행 속도가 구독자의 처리속도보다 크게 빠를 때 사용 (BackPressure Issue)
+        println("==========> Flowable")
+        var integerFlowable = Flowable.just(1, 2, 3, 4,5, 6)
+        integerFlowable.subscribe(::println)
+
+        integerFlowable.subscribe(
+            { println("2 : ${it}")}
+        )
+
+        // 2. 배압처리 안됨
+        println("==========> Observable")
+        var integerObservable = Observable.just(1, 2, 3, 4,5, 6)
+        integerObservable.subscribe(::println)
+    }
+
+}
+
+fun main() {
+    var rxJava01 = RxJava01()
+    rxJava01.basicConcept01()
+}
+
